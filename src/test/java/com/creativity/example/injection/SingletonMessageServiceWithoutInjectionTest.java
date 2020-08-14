5620c25c91ffebbd77a64803ba38484afe7c2f91
Hansel <hansel.hir@gmail.com>
2020-08-14 12:59:17 -0500

    test: Se crea test sin aplicar injección.

---
+++ src/test/java/com/creativity/example/injection/SingletonMessageServiceWithoutInjectionTest.java
@@ -0,0 +1,26 @@
+package com.creativity.example.injection;
+
+import org.apache.logging.log4j.LogManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SingletonMessageServiceWithoutInjectionTest {
+
+    private SingletonMessageService singletonMessageService;
+
+    @Before
+    public void before(){
+        //SingletonMessageService singletonMessageService = new SingletonMessageService();
+        this.singletonMessageService = new SingletonMessageService();
+    }
+
+
+    @Test
+    public void testGetMessage(){
+        Assert.assertTrue(this.singletonMessageService.getMessage().length() > 0);
+        LogManager.getLogger(this.getClass()).info("=======>>> NOTE, without injection");
+        LogManager.getLogger(this.getClass()).info("==========>> message: " + this.singletonMessageService.getMessage());
+    }
+
+}
