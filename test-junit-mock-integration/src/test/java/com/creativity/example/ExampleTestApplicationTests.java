801e47525237a4d61ac4a5517841a2dba5712872
Hansel <hansel.hir@gmail.com>
2020-08-18 20:38:14 -0500

    Test: Se crea clase main del test.

--- test-junit-mock-integration/src/test/java/com/creativity/example/ExampleTestApplicationTests.java
+++ test-junit-mock-integration/src/test/java/com/creativity/example/ExampleTestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ExampleTestApplicationTests {
+public class ExampleTestApplicationTests {
 
     @Test
     void contextLoads() {
