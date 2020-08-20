935ac575834ae9ab5daaa10b86f36170e4705472
Hansel <hansel.hir@gmail.com>
2020-08-20 18:24:41 -0500

    Test: Se configura main principal con Mockmvc para test.

--- app-service-hanselnpetal/src/test/java/com/hanselnpetal/ApplicationTest.java
+++ app-service-hanselnpetal/src/test/java/com/hanselnpetal/ApplicationTest.java
@@ -1,19 +1,5 @@
 package com.hanselnpetal;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class ApplicationTest {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
-/*
-package com.hanselnpetal;
-
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -38,8 +24,9 @@ public class ApplicationTest {
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello World")));
+       // this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().is4xxClientError())
+                .andExpect(content().string(containsString("")));
+                //.andExpect(content().string(containsString("Hello World")));
     }
 }
-*/
